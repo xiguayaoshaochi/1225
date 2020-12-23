89686734f5bbe7358a563e7420899b199114fbc1
西瓜要少吃 <yuruweimu@163.com>
2020-12-23 17:00:37 +0800

    33

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 var cjs = createjs;
-  //开始 hahaha123
+  //开始 hahaha12333
   // con_red.filters = [new createjs.ColorFilter(0.45, 0.45, 0.45, 1, 140.25, 13.2, 13.2, 0), new createjs.ColorMatrixFilter(new createjs.ColorMatrix(100, 0, 0, 0))];
   // con_red.filters = [new createjs.ColorFilter(0.45, 0.45, 0.45, 1, 140.25, 13.2, 13.2, 0)];
   // con_red.cache(1100, 600, 400, 400);
